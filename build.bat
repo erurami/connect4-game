d31673468cc224eedb05460a4fa617537a881768
erurami <88187787+erurami@users.noreply.github.com>
2022-03-07 20:59:53 +0700

    build update make builds/ and save build there

--- build.bat
+++ build.bat
@@ -2,9 +2,18 @@
 @echo off
 
 rc WndMenu.rc
-cl main.cpp WndMenu.res /W4 /std:c++17 /EHsc /link /subsystem:windows
+
+rmdir builds /q /s
+mkdir builds
+
+cl main.cpp WndMenu.res /W4 /std:c++17 /EHsc /Fe:builds\main.exe /link /subsystem:windows
+
+cd builds
+
 mt -manifest main.exe.manifest -outputresource:main.exe;1
+del main.exe.manifest
+
+cd ..
 
 del WndMenu.res
-del main.exe.manifest
 
